64b9a77de5c0cd394a3831b4d908222a6fd8fa5b
didotb <itsmedidot@vivaldi.net>
2026-02-20 02:45:59 +0800

    Run script as current account.

--- neovim/neovim-install.sh
+++ neovim/neovim-install.sh
@@ -3,19 +3,18 @@
 archivename=neovim-$(uname -i).tar.gz
 
 # Install pre-requisites
-apt update
-apt install git npm unzip gcc make ripgrep tree-sitter-cli -y
+sudo apt update
+sudo apt install git npm unzip gcc make ripgrep tree-sitter-cli -y
 
 # Download latest neovim
 wget -O $archivename https://github.com/neovim/neovim/releases/latest/download/nvim-linux-$(uname -i).tar.gz
 
 # Extract archive
-Etar xzf $archivename
 mkdir nvim
 tar xzf $archivename --strip 1 -C nvim
 
 # Copy to usr directory
-cp -R ./nvim/* /usr/
+sudo cp -R ./nvim/* /usr/
 
 # Install NvChad
 git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
